6338c6d85cd45c200b940c7e2ec03c4fd47f163c
Joe Fiume <joefiume88@gmail.com>
2017-09-27 14:59:42 -0700

    set up socket functions for name, join, message, rooms

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ const app = express();
 const http = require('http').Server(app);
 const path = require('path');
 
-app.get('/', function(req, res) {
-  res.type('html');
-});
+// const chatServer = require('./lib/chatServer');
+// chatServer.listen(http);
 
 const PORT = 8000;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
